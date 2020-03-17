ff13f1681d26dfc7f15fd3dadd685204f301f56c
jihoon <jihoon_yang@ksmartech.com>
2020-03-17 14:55:37 +0900

    SpringSecurity 추가

--- src/main/java/readinglist/ReadingListController.java
+++ src/main/java/readinglist/ReadingListController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
-	private static final String reader = "craig";
 	
 	private ReadingListRepository readingListRepository;
 	
@@ -21,17 +20,18 @@ public class ReadingListController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String readersBooks(Model model) {
+	public String readersBooks(Reader reader, Model model) {
 		List<Book> readingList = readingListRepository.findByReader(reader);
 		if (readingList!=null) {
 			model.addAttribute("books", readingList);
+			model.addAttribute("reader", reader);
 		}
 		
 		return "readingList";
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String addToReadingList(Book book) {
+	public String addToReadingList(Reader reader, Book book) {
 		book.setReader(reader);
 		readingListRepository.save(book);
 		
